9cab4b714bf2f27d6ee1ccad02a2e5625bc47610
FaniryKeziah <hei.keziah.2@gmail.com>
2024-02-05 16:11:49 +0300

    feat: add Builder

---
+++ src/main/java/builder/Builder.java
@@ -0,0 +1,17 @@
+package builder;
+
+
+import car.CarType;
+import component.Engine;
+import component.GPSNavigator;
+import component.Transmission;
+import component.TripComputer;
+
+public interface Builder {
+    void setCarType(CarType type);
+    void setSeats(int seats);
+    void setEngine(Engine engine);
+    void setTransmission(Transmission transmission);
+    void setTripComputer(TripComputer tripComputer);
+    void setGPSNavigator(GPSNavigator gpsNavigator);
+}
